18a856ed9856456cbe8675dcd23ce49b1c0501e6
WiredG <thomas.nykjaer@live.dk>
2022-03-20 21:51:35 +0100

    final

--- Pages/ECC/ecc.js
+++ Pages/ECC/ecc.js
@@ -1,12 +1,27 @@
 function initMap() {
-    // The location of Uluru
-    const toby = { lat: 57.02459288026477, lng: 9.944473552234317 };
+    const markers = [
+        { lat: 57.05270767455275, lng: 9.913094102327587 },
+        { lat: 57.012554751715186, lng: 9.991338053228736 },
+        { lat: 57.01750002653716, lng: 9.972801777699798 },
+        { lat: 57.017410295776145, lng: 9.972801777699798 }
+    ]
+
+
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: toby,
+        zoom: 12,
+        center: markers[2],
     });
-    // The marker, positioned at Uluru
+    
+    mapMarkers = [];
+
+    for(let i = 0; i < markers.length; i++){
+        new google.maps.Marker({
+            position: markers[i],
+            map: map,
+        });
+    }
+    
     const marker = new google.maps.Marker({
         position: toby,
         map: map,
