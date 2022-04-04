7f2e3adaeae5a9d05538556fc5f2c72e8af48b03
Nicklas Christensen <klint@eripio.xyz>
2022-04-04 14:19:57 +0200

    fetch test

--- Pages/Caller/caller.js
+++ Pages/Caller/caller.js
@@ -1,3 +1,12 @@
+
+
+// General test generator for callers
+// implement form based test (DONE)
+// implement RNG data (NOT DONE)
+
+
+// Takes info written in the caller.html form and assigns it to variables
+// The variables are used to assign data to a caller
 function handleSubmit(event) {
   const data = new FormData(event.target);
 
@@ -15,13 +24,16 @@ function handleSubmit(event) {
   infoPlacer(name, situation, address, number, injuries);
 }
 
+
+// Listens to when a form is submitted, and runs the handlesubmit function
 const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   handleSubmit(event);
   });
 
-  
+
+// Reads info from variables and determines whether there is a need for generating placeholders or not
 function infoPlacer(name, situation, address, number, injuries) {
   let addressArr = ["57.017145, 9.987593", "57.052578, 9.911738", "57.046832, 9.913825"];
   let addressArrIndex
@@ -45,7 +57,8 @@ function infoPlacer(name, situation, address, number, injuries) {
     injuries = "unknown injuries";
   }
 
-  let caller = {
+  // Assigns the info values to an object
+  let callObj = {
     name: name, 
     address: address, 
     situation: situation,  
@@ -59,28 +72,48 @@ function infoPlacer(name, situation, address, number, injuries) {
   }
 
 
-  let test = JSON.stringify(caller)
+  let caller = JSON.stringify(callObj)
   
-  console.log(test);
+  
+
+  sendJSON(callObj);
 
 }
-
 /*
-request.post({
-  url: URL,
-  headers: {
-    this.name = name, 
-    this.address = address, 
-    this.whatIs = whatIs, 
-    this.whenIs = whenIs, 
-    this.phoneNumber = phoneNumber, 
-    this.AMLLocation = AMLLocation, 
-    this.HowManyPeople = HowManyPeople, 
-    this.answered = false, 
-    this.answering = false, 
-    this.useful = true
-  }
+function sendJSON(caller){
+    
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = "Pages/Caller/caller.html";
+
+  // open a connection
+  xhr.open("POST", url, true);
+
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Create a state change callback
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+
+          // Print received data from server
+          result.innerHTML = this.responseText;
+
+      }
+  };
+  console.log("test");
+  // Sending data with the request
+  xhr.send(caller);
+}
 */
-
-
-
+/*
+async function sendJSON(caller) {
+// request options
+const response = await fetch('Pages/Caller/caller', {
+  method: 'POST',
+  body: JSON.stringify(caller)
+});
+const responseText = await response.text();
+console.log(responseText); // logs 'OK'
+}
+*/
