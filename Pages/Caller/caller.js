a436b146718f1b23a9bce4da8c965ac797ba6bcd
WiredG1 <thomas.nykjaer@live.dk>
2022-04-25 11:34:05 +0200

    Formatting and splitting

--- Pages/Caller/caller.js
+++ Pages/Caller/caller.js
@@ -9,35 +9,10 @@
 
 // PLACEHOLDER FUNCTION 
 // NEEDED FOR GOOGLE MAPS LAT AND LONG
-function add_geo_marker(address) {
-  return new Promise((resolve, reject) => {
-    let latlngArr = [];
-    let latitude;
-    let longitude;
-    let latlngObj;
-    // Creates new geocoder which allows us to convert a standard adress to LAT and LNG
-    let geocoder = new google.maps.Geocoder();
-    // geocode is an api, which Converts the "standard" address to LAT and LNG
-    geocoder.geocode({ 'address': address }, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        latlngArr[0] = (results[0].geometry.location.lat());
-        latlngArr[1] = (results[0].geometry.location.lng());
-        latitude = ((results[0].geometry.location.lat()));
-        latlngObj = {
-          lat: ((results[0].geometry.location.lat())),
-          lng: ((results[0].geometry.location.lng()))
-        }
-        resolve(latlngObj);
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-        reject("Unknown address");
-      }
-    });
-  });
-}
 
 // Takes info written in the caller.html form and assigns it to variables
 // The variables are used to assign data to a caller
+
 function handle_submit(event) {
   const data = new FormData(event.target);
 
@@ -54,20 +29,10 @@ function handle_submit(event) {
   info_placer(name, situation, address, injuries, description);
 }
 
-
-// Listens to when a form is submitted, and runs the handlesubmit function
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  handle_submit(event);
-});
-
-
 // Reads info from variables and determines whether there is a need for generating placeholders or not
 function info_placer(name, situation, address, injuries, description) {
   let addressArr = [{ lat: 57.017145, lng: 9.987593 }, { lat: 57.052578, lng: 9.911738 }, { lat: 57.046832, lng: 9.913825 }];
   let addressArrIndex, formZeroLen = 0, numberMAX = 99999999, numberMIN = 10000000;
-  let locationObj;
   let tempNumber = Math.floor(Math.random() * numberMAX);
   let latlngObj;
   tempNumber < numberMIN ? number = tempNumber + 10000000 : number = tempNumber;
@@ -93,27 +58,6 @@ function info_placer(name, situation, address, injuries, description) {
     // Assigns the info values to an object
     clear_form();
   }
-}
-// Creates a POST request with the caller object
-// Used for caller queue and other
-// Stored in ServerData/CallerDB/
-// sendJSON
-async function send_JSON(caller) {
-  let response = await fetch('/callerobj', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: caller,
-  });
-}
-
-function clear_form() {
-  document.getElementById('name').value = '';
-  document.getElementById('situation').value = '';
-  document.getElementById('address').value = '';
-  document.getElementById('injuries').value = '';
-  document.getElementById('description').value = '';
 }
 
 function info_placer_result(name, situation, injuries, description, address, latlngObj, addressArr, addressArrIndex) {
