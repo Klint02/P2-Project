1e7826a41f0345914d78aba0becb6232e2aa732e
WiredG1 <thomas.nykjaer@live.dk>
2022-05-09 12:17:27 +0200

    spelling fix

--- Pages/Caller/placeholderDB.js
+++ Pages/Caller/placeholderDB.js
@@ -1,6 +1,6 @@
 let nameDB = ["Jonas Hansen", "Tim Jensen", "Laura Hansen", "Helena bach", "Tobias Madsen", "Rasmus Nørfjand", "Niklas Klint", "Patrick Thomsen", "Thomas Thomsen", "Albert Knudsen", "Steen Fredriksen", "Christian Rasmussen", "Kristian Bertelsen", "Thor Madsbjerg", "Storm Henriksen", "Lars Nygaard", "Andreas Hougaard", "Laura Dalum", "Marie Christensen", "Sofie Chrisitansen", "Hanne Jensen", "Kirsten Kongerslev", "Amanda Skou", "Stine Vangsy", "Karina Drost", "Katrine Mathiasen", "Helena Sejersen", "Line Bornstein", "Julie Gertsen", "Signe Bro", "Karoline Bertelsen", "Louise Andersen", "Tilde Thagaard"]
 
-let situationDB = ["Fire", "Trafic Accident", "Homicide", "Theft", "Breake-in"]
+let situationDB = ["Fire", "Trafic Accident", "Homicide", "Theft", "Break-in"]
 
 let addressDB = ["Maren Turis gade 3", "Strandvejen 3", "Strandvejen 4", "Strandvejen 15", "Dannebrogsgade 25", "Dannebrogsgade 36", "Fredensgade 5", "Fredensgade 15 ", "Granvej 3", "Granvej 7", "Sigrid undsets vej 234B", "Max Henius vej 7", "Max Henius vej 26", "Tjørnevej 12", "Tjørnevej 6", "Tjørnevej 13", "Unknown address"]
 
